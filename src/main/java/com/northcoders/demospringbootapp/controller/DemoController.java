b3e2ba3de4f06ff7beb2d1a4a6c45154300564d8
Shamar <Shamar098@gmail.com>
2024-11-26 12:19:26 +0000

    feat: add implementation to find longitude and latitude of a city parameter via open-meteo

--- src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
+++ src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
@@ -1,23 +1,29 @@
 package com.northcoders.demospringbootapp.controller;
 
+import com.northcoders.demospringbootapp.dao.CityInformationDAO;
 import com.northcoders.demospringbootapp.model.Person;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.northcoders.demospringbootapp.model.Results;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/api/v1")
 public class DemoController {
 
     @GetMapping("/hello")
-    public static String hello(){
+    public static String hello() {
         return "Hello there!";
     }
 
-    Person bob = new Person("Bob Belcher",46,"bob@aol.com",
-            "USA","Burgers");
+    Person bob = new Person("Bob Belcher", 46, "bob@aol.com",
+            "USA", "Burgers");
 
     @GetMapping("/person")
-    public  Person sendBob(){
+    public Person sendBob() {
         return bob;
+    }
+
+    @GetMapping("/citydetails")
+    public Results getCityDetails(@RequestParam String name){
+        return CityInformationDAO.cityLongitudeAndLatitude(name);
     }
 }
