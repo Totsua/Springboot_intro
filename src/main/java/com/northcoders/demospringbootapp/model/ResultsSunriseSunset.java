3f0e592862a47b8a110b193c8e12ba7793bbb2fc
Shamar <Shamar098@gmail.com>
2024-11-26 12:34:57 +0000

    feat: add ResultsSunriseSunset and SunriseSunset records

---
+++ src/main/java/com/northcoders/demospringbootapp/model/ResultsSunriseSunset.java
@@ -0,0 +1,4 @@
+package com.northcoders.demospringbootapp.model;
+
+public record ResultsSunriseSunset(SunriseSunset results) {
+}
---
+++ src/main/java/com/northcoders/demospringbootapp/model/SunriseSunset.java
@@ -0,0 +1,5 @@
+package com.northcoders.demospringbootapp.model;
+
+public record SunriseSunset(String date,
+                            String sunrise,
+                            String sunset) {}
